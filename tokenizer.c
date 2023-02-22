0f091ede3a6f4ddbc7a9d652576c6abf3dc8cbbb
Tonny Kirwa <tonnykirwa@yahoo.com>
2023-02-22 09:46:16 -0800

    tokenizer

--- tokenizer.c
+++ tokenizer.c
@@ -3,7 +3,7 @@
 /**
  * **strtow - splits a string into words. Repeat delimiters are ignored
  * @str: the input string
- * @d: the delimeter string
+ * @d: the delimiter string
  * Return: a pointer to an array of strings, or NULL on failure
  */
 
@@ -35,14 +35,14 @@ k++;
 s[j] = malloc((k + 1) * sizeof(char));
 if (!s[j])
 {
-for (k = 0; k < j; k++)
-free(s[k]);
+for (m = 0; m < j; m++)
+free(s[m]);
 free(s);
 return (NULL);
 }
 for (m = 0; m < k; m++)
 s[j][m] = str[i++];
-s[j][m] = 0;
+s[j][m] = '\0';
 }
 s[j] = NULL;
 return (s);
@@ -51,7 +51,7 @@ return (s);
 /**
 * **strtow2 - splits a string into words
 * @str: the input string
-* @d: the delimeter
+* @d: the delimiter
 * Return: a pointer to an array of strings, or NULL on failure
 */
 char **strtow2(char *str, char d)
